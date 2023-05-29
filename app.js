fb93d71f987db8fd4ccaec29f4ad11b75fe5bfb9
Vinayak-Sutar <sutarvinayak493@gmail.com>
2023-05-29 17:20:09 +0530

    changed port to env

--- app.js
+++ app.js
@@ -268,8 +268,10 @@ async function main() {
 
 
 
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
+const PORT = process.env.PORT || 3000 ;
+
+app.listen(PORT, function() {
+  console.log("Server started on port "+PORT);
 });
 
 
